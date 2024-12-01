fe936d45f4dc28f28df7e746f8a56a0bc7b5a242
Joydeb roy <104813131+JoydebRoy01@users.noreply.github.com>
2024-12-01 22:34:02 +0530

    Add files via upload

---
+++ Assignment2/work.py
@@ -0,0 +1,9 @@
+time=int(input("Enter time to campany job:"))
+if time>2 and time <=3:
+    print("high efficiency")
+elif time>3 and time<=4:
+    print("improve efficiency")
+elif time >=4 and time <=5:
+    print("Give Training")
+else:
+    print("layoff the company")
